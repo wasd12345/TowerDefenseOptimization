bf70a5f5e9da9bdf7036d277dc439151d8360d13
wasd12345 <uy65askvg23o987gli2ue97lu23i@gmail.com>
2018-05-08 19:47:51 -0400

    finished form

--- TowerDefense.py
+++ TowerDefense.py
@@ -5,6 +5,13 @@ Created on Sat Mar 31 21:19:36 2018
 @author: GK
 
 
+Python 2.7.14 |Anaconda custom (64-bit)| (default, Nov  8 2017, 13:40:45) [MSC v
+.1500 64 bit (AMD64)] on win32
+cvxpy.__version__ = 0.4.8
+numpy.__version__ = 1.12.1
+sklearn.__version__ = 0.19.1
+matplotlib.__version__ = 2.1.2
+
 
 
 EE 4650 Convex Optimization Final Project:
@@ -21,7 +28,7 @@ structures.
 Several variations:
     - with obstacles that block line of sight
     - different kinds of towers with different coverage functions [e.g. 
-    different range or different range profile]
+    different ranges / different range profile]
     - different costs of towers, with a fixed overall budget
     - different quotas on kinds of towers
     - different values of structures you want to protect, maximizing the 
@@ -48,21 +55,36 @@ from sklearn.metrics.pairwise import pairwise_distances
 class Environment():
     
     def __init__(self,params_dict):
-        
-        self.penalty_type = params_dict['penalty_type']
-        self.objective_type = params_dict['objective_type']
 
         self.random_seed = params_dict['random_seed']
+        
+        #Problem formulation        
+        self.objective_type = params_dict['objective_type']
+        self.penalty_type = params_dict['penalty_type']
+        self.constraints__type = params_dict['constraints__type']
+        
+        #If doing the variation of the problem with cost budgets:
+        self.budget__total_cost = params_dict['budget__total_cost'] if\
+        'budget__total_cost' in params_dict else None
+        self.budget__tower_quotas = params_dict['budget__tower_quotas'] if\
+        'budget__tower_quotas' in params_dict else None        
+        self.budget__tower_unit_costs = params_dict['budget__tower_unit_costs'] if\
+        'budget__tower_unit_costs' in params_dict else None         
+        
+        #Map parameters
         self.map_dimensions = params_dict['map_dimensions']#height(y),width(x)
         self.BORDER_MARGIN = params_dict['BORDER_MARGIN'] #For visualizing / debugging purposes make easier to see placements by not having on edges
         self.N_obstacles = params_dict['N_obstacles']
+        
+        #Target parameters
         self.N_targets = params_dict['N_targets']
         self.target_values = params_dict['target_values']
         
-        
+        #Tower parameters
         self.N_tower_sites = params_dict['N_tower_sites']
         self.N_towers = params_dict['N_towers']
         self.N_tower_kinds = len(self.N_towers)
+        self.coverage_profile_types = params_dict['coverage_profile_types']
         
         #If manually specifying map:
         self.coordinates__obstacles = params_dict['coordinates__obstacles'] if\
@@ -72,23 +94,23 @@ class Environment():
         self.coordinates__tower_sites = params_dict['coordinates__tower_sites'] if\
         'coordinates__tower_sites' in params_dict else None        
 
+        #Variables created to make coverage / protection matrices:
         self.obstructed_masks = []
         self.distances_towers_targets = []
         self.unobstructed_coverages = []
         self.coordinates__solved_towers = []
-        
         self.coverage_matrices = None
         
-        self.budget__total_cost = 100
-        self.budget__tower_quotas = [999,999]
-        self.budget__tower_unit_costs = [2,3]
-        
-        
-        self.N_iterations_max = params_dict['N_iterations_max'] #Number iterations of Weighted L1. For this problem, seems to converge pretty fast (only a few iterations)
-        
-        self.coverage_profile_types = params_dict['coverage_profile_types']
+
         self.figsize = (18,12)
         
+        
+        #Number iterations of Weighted L1. For this problem, seems to converge pretty fast (only a few iterations)
+        self.N_reweighting_iterations_max = params_dict['N_reweighting_iterations_max']         
+        #Number of random independent starts (since we are not gauranteed to 
+        #reach a global optimum, try a few times, and take the best result)
+#        self.N_random_starts_max = params_dict['N_random_starts_max']         
+        #CVXPY Optimizer output
         self.VERBOSE = params_dict['VERBOSE']
         
         def summarize_environment():
@@ -362,73 +384,103 @@ class Environment():
         print coverage.shape 
         
         #Diagonal matrix of the values of each target
+        #(for the scenarios where we don't care about maximizing covered value,
+        #target_values is just all ones, so this is just the identity matrix)
         V = np.diag(self.target_values)
         
-        #Get the ad
+        #If doing scenario where we want to fortify weakest link, only makes
+        #sense if all targets are equal value:
+        if self.objective_type == 'min_entries':
+            V = np.eye(len(self.target_values))
+
+        #Get the matrix of coverage values / expected value saved:
         C = np.dot(V,coverage)
         print 'V', V
         print 'coverage', coverage
         print 'C', C
         
         
-        method = 2
+        #Since not gauranteed to reach global optimum on any particular initialization,
+        #run a few times and take the best result.
+        #Just define "best result" as the result which had the most overall 
+        #"converged" x, combined over all tower kinds. 
+#        for j in xrange(self.N_random_starts_max):
+            
         
-        if method == 2:
-            a = 2. #1.
-            tau = 1e-4
-            N = sum(i for i in self.N_tower_sites)
-    #            print N
-            w = np.zeros(N)
-            ones = np.ones(N)
-            p = 1.
-            for i in xrange(self.N_iterations_max):
-                #The concatenated vector of occupancies: Concatenated over all
-                #of the kinds of towers.
-                x = cvx.Variable(N)
+        a = 2. #1.
+        tau = 1e-4
+        N = sum(i for i in self.N_tower_sites)
+        w = np.zeros(N)
+        ones = np.ones(N)
+        p = 1. #the exponents power when doing he exponent method:
+        
+        for i in xrange(self.N_reweighting_iterations_max):
+            #The concatenated vector of occupancies: Concatenated over all
+            #of the kinds of towers.
+            x = cvx.Variable(N)
+            
+            #Different objective functions depending on which optimization problem.
+            #These are defined in the scenarios in the main function.
+            if self.objective_type == 'min_entries':
+                operation = cvx.min_entries
+            elif self.objective_type == 'sum_entries':
+                operation = cvx.sum_entries
+            else:
+                raise Exception('must specify valid objective_type')
                 
-                if self.objective_type == 'min_entries':
-                    operation = cvx.min_entries
-                elif self.objective_type == 'sum_entries':
-                    operation = cvx.sum_entries
-                elif self.objective_type == 'max_entries':
-                    operation = cvx.max_entries
-                else:
-                    raise Exception('must specify objective_type')
-                    
-                #Objective function includes penalty term for non-binary x values
-                if self.penalty_type == 'reweighted_L1':
-                    #objective = cvx.Maximize(t - x.T*w)
-                    objective = cvx.Maximize(operation(C*x - x.T*w))
-                elif self.penalty_type == 'exponential':
-                    objective = cvx.Maximize(operation(C*x - ((x.T)**p)*ones))
-    
-                #Main constraints on 0<=x<=1 and on t
-                constraints = [0<=x, x<=1]
-                #And then for each kind of tower, append the constraint that there
-                #be exactly N_i towers 
+            #Objective function includes penalty term for non-binary x values
+            if self.penalty_type == 'reweighted_L1':
+                #objective = cvx.Maximize(t - x.T*w)
+                objective = cvx.Maximize(operation(C*x - x.T*w))
+
+
+            #Main constraints on 0<=x<=1
+            constraints = [0<=x, x<=1]
+            
+            
+            #And then for each kind of tower, append the constraint that there
+            #be exactly N_i towers, or <= quota (depending on constraint type)
+            if self.constraints__type == 'fixed_N_towers' or self.constraints__type == 'tower_quotas':
                 for tk in xrange(self.N_tower_kinds):
                     before_sum = np.concatenate(([0],np.cumsum(self.N_tower_sites)))[tk]
                     print before_sum
                     print before_sum + self.N_tower_sites[tk]
-                    constraints.append(cvx.sum_entries(
-                                    x[before_sum : before_sum + self.N_tower_sites[tk]]
-                                    )==self.N_towers[tk])
+                    if self.constraints__type == 'fixed_N_towers':
+                        constraints.append(cvx.sum_entries(
+                                        x[before_sum : before_sum + self.N_tower_sites[tk]]
+                                        )==self.N_towers[tk])
+                    elif self.constraints__type == 'tower_quotas':
+                        constraints.append(cvx.sum_entries(
+                                        x[before_sum : before_sum + self.N_tower_sites[tk]]
+                                        )<=self.budget__tower_quotas[tk])
                     print x[before_sum : before_sum + self.N_tower_sites[tk]]
-    
-                print 'penalty_type', self.penalty_type
-                print 'objective_type', self.objective_type
-                print 'operation', operation
-                print 'objective', objective
-                print 'constraints', constraints
-                cvx.Problem(objective, constraints).solve(verbose=self.VERBOSE)
-                x = np.array(x.value).flatten()
-                print 'x', x
-                w = a/(tau+np.abs(x))
-                p += 1.
-                plt.figure(figsize=(5,5))
-                plt.plot(x,marker='o')
-                plt.savefig('histrograms_{}.png'.format(i))    
-                print            
+                    
+            elif self.constraints__type == 'total_cost':
+                costs = np.hstack([np.repeat(self.budget__tower_unit_costs[tk],self.N_tower_sites[tk]) for tk in xrange(self.N_tower_kinds)])
+                constraints.append(cvx.sum_entries(costs * x) <= self.budget__total_cost)                    
+                    
+                    
+                
+
+
+                
+                
+            print 'penalty_type', self.penalty_type
+            print 'objective_type', self.objective_type
+            print 'constraints__type', self.constraints__type
+            print 'budget__tower_quotas', self.budget__tower_quotas
+            print 'operation', operation
+            print 'objective', objective
+            print 'constraints', constraints
+            cvx.Problem(objective, constraints).solve(verbose=self.VERBOSE)
+            x = np.array(x.value).flatten()
+            print 'x', x
+            w = a/(tau+np.abs(x))
+            p += 1.
+            plt.figure(figsize=(5,5))
+            plt.plot(x,marker='o')
+            plt.savefig('histrograms_{}.png'.format(i))
+            print            
             
             
             
@@ -436,7 +488,6 @@ class Environment():
         #From the solution x, get the coordinates of those tower sites where we
         #really do want to place a tower
         #use = np.isclose(x,1.)
-
         for tk in xrange(self.N_tower_kinds):
             before_sum = np.concatenate(([0],np.cumsum(self.N_tower_sites)))[tk]
             y = x[before_sum : before_sum + self.N_tower_sites[tk]]
@@ -446,14 +497,24 @@ class Environment():
             print inds
             print s
             print use            
-            if len(use) != self.N_towers[tk]:
-                print 'Solution did not converge properly. Choosing the K best towers.'
-                print self.N_towers[tk], len(use)
-            use = use[-self.N_towers[tk]:]
+            if self.constraints__type == 'fixed_N_towers':
+                if len(use) != self.N_towers[tk]:
+                    print 'Solution did not converge properly. Choosing the K best towers.'
+                    print self.N_towers[tk], len(use)
+    #                use = use[-self.N_towers[tk]:]
+                    use = inds[-self.N_towers[tk]:]
+            elif self.constraints__type == 'tower_quotas':
+                pass #Just use the towers thresholded at > .5
             print use
+            
+            
             self.coordinates__solved_towers.append([self.coordinates__tower_sites[tk][mm] for mm in inds[use]])
-#        print self.coordinates__solved_towers
-#        print len(self.coordinates__solved_towers)
+    #        print self.coordinates__solved_towers
+    #        print len(self.coordinates__solved_towers)
+        
+    
+    
+    
     
     def run_scenario(self):
         """
@@ -479,6 +540,9 @@ if __name__ == '__main__':
     
     
     
+    #'objective_type':  'sum_entries' vs. 'min_entries':
+    #min corresponds to the problem of fortifying the weakest link
+    #sum gives makes objective function maximize the total overall coverage.
     
     
     
@@ -487,16 +551,22 @@ if __name__ == '__main__':
     # =============================================================================
     #     MAIN
     # =============================================================================    
-    
     #SCENARIO !:
     #Explanation:
     #asdasdasdad
     if SCENARIO == 1:
-        NTARGETS = 38
-        params_dict = {'random_seed':3312018,
+        NTARGETS = 33
+        params_dict = {'random_seed':33120181,
                        'objective_type':'sum_entries',#'min_entries','sum_entries'
-                       'penalty_type':'reweighted_L1',#'exponential','reweighted_L1'
-                       'N_iterations_max':4,
+                       'penalty_type':'reweighted_L1',#'exponential','reweighted_L1','double_square'
+                       'constraints__type': 'total_cost',#'fixed_N_towers', 'tower_quotas', 'total_cost'
+                       #If want to include quotas / budgets on towers:
+                       'budget__total_cost': 30.,
+                       'budget__tower_quotas': [1,1,1],#[6,3,2],#If don't want to limit number of a given tower type, just put in large value 999999
+                       'budget__tower_unit_costs': [50.,50.,20.],
+
+                       'N_reweighting_iterations_max':10,
+#                       'N_random_starts_max':5,
                        'map_dimensions':(300,400),
                        'BORDER_MARGIN':3,
                        'N_obstacles':10,
@@ -505,9 +575,9 @@ if __name__ == '__main__':
                        'target_values': [9999.] + [1.]*(NTARGETS-1),
 #                       'target_values': [1.]*NTARGETS,
                        'target_health': [10.]*NTARGETS,#!!!!!!!!!!!!!!!!not implemented yet to turn into probability protected
-                       'N_tower_sites':[733,5,7],#[10,13],
-                       'N_towers':[9,2,3],#[3,4],
-                       'coverage_profile_types':['fixed_radius','fixed_radius','fixed_radius'],#['fixed_radius','fixed_radius'], #['fixed_radius','inverse_r','inverse_r2']
+                       'N_tower_sites':[27,15,17],#[10,13],
+                       'N_towers':[9,7,5],
+                       'coverage_profile_types':['inverse_r','inverse_r','inverse_r'], #['fixed_radius','inverse_r','inverse_r2']
     #                   'coordinates__obstacles':[(0,0,44,44)],
     #                   'coordinates__targets':[(0,0),(88,66)],
     #                   'coordinates__tower_sites':[(99,107),(377,288)]
